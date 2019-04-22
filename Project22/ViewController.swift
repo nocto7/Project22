9ecb94a14dd1d3c72760c4402d4781dec40f26b0
Kirsty Darbyshire <kirsty@loquax.com>
2019-04-22 17:58:04 +0100

    tidying up

--- Project22/ViewController.swift
+++ Project22/ViewController.swift
@@ -12,7 +12,7 @@ import CoreLocation
 class ViewController: UICollectionViewController, CLLocationManagerDelegate {
 
     var locationManager: CLLocationManager?
-    var beaconInformation = [BeaconInfo]()
+    var beaconInfo = [BeaconInfo]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +20,6 @@ class ViewController: UICollectionViewController, CLLocationManagerDelegate {
         locationManager = CLLocationManager()
         locationManager?.delegate = self
         locationManager?.requestAlwaysAuthorization()
-        
-        // set a default nothing has been found cell here probably...
     }
 
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -35,43 +33,47 @@ class ViewController: UICollectionViewController, CLLocationManagerDelegate {
     }
     
     func startScanning() {
-        addBeacon(uuid: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5", name: "5A4", major: 123, minor: 456)
+        addBeacon(uuid: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5", name: "Apple 5A4", major: 123, minor: 456)
         addBeacon(uuid: "92AB49BE-4127-42F4-B532-90fAF1E26491", name: "TwoCanoes", major: 987, minor: 654)
+        addBeacon(uuid: "5AFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF", name: "RedBear", major: 456, minor: 789)
         collectionView.reloadData()
     }
     
     func addBeacon(uuid uuidString: String, name: String, major: CLBeaconMajorValue, minor: CLBeaconMinorValue) {
         let uuid = UUID(uuidString: uuidString)!
         let region = CLBeaconRegion(proximityUUID: uuid, major: major, minor: minor, identifier: "\(name) beacon")
-        beaconInformation.append(
+        beaconInfo.append(
             BeaconInfo(uuid: uuid, shortName: name, region: region, detected: false, proximity: .unknown))
         locationManager?.startMonitoring(for: region)
         locationManager?.startRangingBeacons(in: region)
     }
     
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
-        if let beacon = beacons.first {
-            print("beacon \(beacon.proximityUUID) has been detected")
-            // find beacon info with this uuid
-            for possibleBeacon in beaconInformation {
+        for possibleBeacon in beaconInfo {
+            var currentlyFound = false
+            for beacon in beacons {
                 if possibleBeacon.uuid == beacon.proximityUUID {
-                    print("matched beacon \(possibleBeacon)")
+                    currentlyFound = true
                     possibleBeacon.detected = true
                     possibleBeacon.proximity = beacon.proximity
                     possibleBeacon.delegate?.update(distance: beacon.proximity)
                     //collectionView.reloadData()
                 }
             }
+            if !currentlyFound {
+                possibleBeacon.detected = false
+                possibleBeacon.proximity = .unknown
+            }
         }
     }
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return beaconInformation.count
+        return beaconInfo.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "beacon", for: indexPath) as? BeaconCell else { fatalError("Unable to dequeue a beacon cell") }
         cell.setup()
-        cell.beaconInfo = beaconInformation[indexPath.row]
+        cell.beaconInfo = beaconInfo[indexPath.row]
         cell.beaconInfo.delegate = cell
         cell.update(distance: cell.beaconInfo.proximity)
         return cell
